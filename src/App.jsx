3e7370a8e57a2d168d96a120bbaa56f907547156
unknown <antonio.bermudez.ing@gmail.com>
2022-03-24 17:12:31 -0600

    Configuacion para la aplicacion

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Header from "./components/Header"
+import Form from "./components/Form"
+import ListadoPacientes from "./components/ListadoPacientes"
+function App() {
+
+  return (
+    <>
+      <Header />
+      <Form />
+      <ListadoPacientes />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,7 @@
+function Header() {
+    return (
+        <h1>Desde el Header</h1>
+    )
+}
+
+export default Header;
